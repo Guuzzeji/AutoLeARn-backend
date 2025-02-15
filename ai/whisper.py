7b0ef19b0d55bbc95c5d0b5db4828d5f6e856b67
Guuzzeji <5lh8dl440f@gmail.com>
2025-02-15 10:40:39 -0800

    setting up whisper endpoint, need to test this

---
+++ ai/whisper.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from groq import Groq
+
+client = Groq()
+
+def whisper(file_path: str) -> dict[str]:
+    try:
+        with open(file_path, "rb") as file:
+            transcription = client.audio.transcriptions.create(
+                file=(file_path, file.read()),
+                model="whisper-large-v3-turbo",
+                language="en",
+                response_format="json",
+                temperature=0.0,
+            )
+        return {"transcript": transcription.text}
+    except Exception as e:
+        print(e)
+        return None
--- routes/api.py
+++ routes/api.py
@@ -8,7 +8,23 @@ API_PATH = Blueprint("api", __name__, url_prefix="/api")
 FILE_DUMP = "TEMP_SAVE_FOLDER"
 os.makedirs(FILE_DUMP, exist_ok=True)
 
-
 @API_PATH.route("/ping", methods=["GET"])
 def handle_ping():
     return jsonify({"message": "pong"}), 200
+
+
+@API_PATH.route("/transcribe", methods=["POST"])
+def handle_transcribe():
+    if "audio" not in request.files:
+            return jsonify({"error": "No audio file provided"}), 400
+
+    audio_file = request.files["audio"]
+    file_path = os.path.join(FILE_DUMP, audio_file.filename)
+    audio_file.save(file_path)
+
+    response = whisper(file_path)
+
+    if response == None:
+        return jsonify({"error": "Failed to transcribe audio"}), 500
+    
+    return jsonify(response), 200
