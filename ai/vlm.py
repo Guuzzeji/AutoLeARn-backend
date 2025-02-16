a9b41f87b301d1d1550ac4135045a911e1a1866d
Guuzzeji <5lh8dl440f@gmail.com>
2025-02-16 00:05:12 -0800

    bugfix: prompt in func vlm

--- ai/vlm.py
+++ ai/vlm.py
@@ -8,41 +8,6 @@ load_dotenv()
 
 client = Groq()
 
-# NOTE (Gabe): cannot use system prompt for vlm models
-USER_PROMPT_TEMPLATE = {
-    "role": "user",
-    "content": [
-        {
-            "type": "text",
-            "text":
-            # NOTE (Gabe): This is good enough, but i feel like we can improve more
-            """
-            You are an AI automotive repair assistant with visual recognition capabilities. 
-            Your role is to analyze user-submitted images and provide clear, objective descriptions 
-            to assist in vehicle diagnostics and repair. All images given to you is the user's car.
-
-            Your goal is to describe the user's vehicle's condition, identify potential issues within 
-            the image, focus on key details.
-
-            Use the user additional context to provide context about the user's vehicle. 
-            If it align with the image given to you.
-            """
-        },
-        {
-            "type": "text",
-            "text": "USER ADDITIONAL CONTEXT: "  # user context
-        },
-        {
-            "type": "image_url",
-            # NOTE: image has to be correct / same type, GROQ get assy when not using correct image data
-            "image_url":
-            {
-                "url": "data:image/jpeg;base64,",  # base64 encoded image
-            },
-        },
-    ],
-}
-
 
 def image_to_base64(image_path):
     with open(image_path, "rb") as image_file:
@@ -50,11 +15,43 @@ def image_to_base64(image_path):
 
 
 def vlm(image_path: str, nl: str) -> dict[str] or None:
+    user_prompt = {
+        "role": "user",
+        "content": [
+            {
+                "type": "text",
+                "text":
+                # NOTE (Gabe): This is good enough, but i feel like we can improve more
+                """
+                You are an AI automotive repair assistant with visual recognition capabilities. 
+                Your role is to analyze user-submitted images and provide clear, objective descriptions 
+                to assist in vehicle diagnostics and repair. All images given to you is the user's car.
+
+                Your goal is to describe the user's vehicle's condition, identify potential issues within 
+                the image, focus on key details.
+
+                Use the user additional context to provide context about the user's vehicle. 
+                If it align with the image given to you.
+                """
+            },
+            {
+                "type": "text",
+                "text": "USER ADDITIONAL CONTEXT: "  # user context
+            },
+            {
+                "type": "image_url",
+                # NOTE: image has to be correct / same type, GROQ get assy when not using correct image data
+                "image_url":
+                {
+                    "url": "data:image/jpeg;base64,",  # base64 encoded image
+                },
+            },
+        ],
+    }
+
     try:
-        user_prompt = copy.deepcopy(USER_PROMPT_TEMPLATE)
         user_prompt["content"][1]["text"] += nl
-        user_prompt["content"][2]["image_url"]["url"] += image_to_base64(
-            image_path)
+        user_prompt["content"][2]["image_url"]["url"] += image_to_base64(image_path)
 
         chat_completion = client.chat.completions.create(
             model="llama-3.2-90b-vision-preview",
